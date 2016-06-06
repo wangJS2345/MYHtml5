777faadae4d14e80ff4a41053508d0ad23e0fa8a
e2 <wangjs2345@163.com>
2016-06-06 09:38:00 +0800

    编辑html文件，修改画面

--- MYHtml5/ViewController.m
+++ MYHtml5/ViewController.m
@@ -23,5 +23,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)refreshClick:(UIButton *)sender {
+    [self reloadInputViews];
+}
 
 @end
